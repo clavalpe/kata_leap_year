e7f84fbdca89e29a279ae5d706df55a04cc930da
Clara <clavalpe@upv.es>
2024-11-02 15:05:19 +0100

    refactoring

--- src/leap_year.py
+++ src/leap_year.py
@@ -2,6 +2,8 @@ class LeapYearValidator:
     def check(self, year: int) ->  bool:
         if year % 4 != 0:
             return False
+        
         if year % 100 == 0 and year % 400 != 0:
             return False
+        
         return True
