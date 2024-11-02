97fa3c31300e83774d166745b9e0b8a8b82e3c44
Clara <clavalpe@upv.es>
2024-11-02 13:36:48 +0100

    red - if divisible by 4 is leap year

--- src/test/test_leap_year.py
+++ src/test/test_leap_year.py
@@ -1,6 +1,11 @@
+import pytest
 from leap_year import LeapYearValidator
 
 
 class TestLeapYearValidator:
-    def test_if_not_divisible_by_4_is_not_leap_year(self):
-        assert LeapYearValidator().check(1997) == False
+    @pytest.mark.parametrize("year", [1997, 1995, 1991])
+    def test_if_not_divisible_by_4_is_not_leap_year(self, year):
+        assert LeapYearValidator().check(year) == False
+
+    def test_if_divisible_by_4_is_leap_year(self):
+        assert LeapYearValidator().check(2024) == True
